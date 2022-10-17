56305fa527d6f07dc0213a840cf945549a372cb0
LittleBear <113894585+LittleBear4@users.noreply.github.com>
2022-10-17 10:22:16 +0800

    Update main.py

--- main/Anywhere/main.py
+++ main/Anywhere/main.py
@@ -15,13 +15,13 @@ def main(target_url):
         target_url = 'http://' + target_url
     if target_url[-1] != '/':
         target_url += '/'
-        list = ['通达OA_v11_5_swfupload_sql', '通达OA_v11_5_任意用户登录','通达OA_v11_6_insert_sql',
-                '通达OA_v11_6_report_bi_sql', '通达OA_v11_6_任意文件删除_RCE', '通达OA_v11_7_后台sql注入',
-                '通达OA_v11_7_在线用户登录', '通达OA_v11_8_api_任意文件上传', '通达OA_v11_8_getway_远程文件包含',
-                '通达OA_v2014_get_contactlist', '通达OA_v2017_action_upload', '通达OA_v2017_任意用户登录']
-        for i in list:
-            eval(i + ".main(target_url)")
-            time.sleep(0.2)
+    list = ['通达OA_v11_5_swfupload_sql', '通达OA_v11_5_任意用户登录','通达OA_v11_6_insert_sql',
+            '通达OA_v11_6_report_bi_sql', '通达OA_v11_6_任意文件删除_RCE', '通达OA_v11_7_后台sql注入',
+            '通达OA_v11_7_在线用户登录', '通达OA_v11_8_api_任意文件上传', '通达OA_v11_8_getway_远程文件包含',
+            '通达OA_v2014_get_contactlist', '通达OA_v2017_action_upload', '通达OA_v2017_任意用户登录']
+    for i in list:
+        eval(i + ".main(target_url)")
+        time.sleep(0.2)
     
 if __name__ == '__main__':
     console.print(Figlet(font='slant').renderText('Office Anywhere OA-Exp'), style='bold blue')
