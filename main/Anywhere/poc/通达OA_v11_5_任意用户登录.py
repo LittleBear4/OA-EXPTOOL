fe3717c5b9aef029d1089acc82793a7a643ca3a9
LittleBear <113894585+LittleBear4@users.noreply.github.com>
2022-11-17 10:14:32 +0800

    Update 通达OA_v11_5_任意用户登录.py

--- main/Anywhere/poc/通达OA_v11_5_任意用户登录.py
+++ main/Anywhere/poc/通达OA_v11_5_任意用户登录.py
@@ -40,7 +40,7 @@ def main(target_url):
             else:
                 console.print(now_time() + " [WARNING]  不存在通达OA_v11.5_任意用户登录漏洞", style='bold red')
         else:
-                console.print(now_time() + " [WARNING]  不存在通达OA_v11.5_任意用户登录漏洞", style='bold red')
+            console.print(now_time() + " [WARNING]  不存在通达OA_v11.5_任意用户登录漏洞", style='bold red')
     except: 
         console.print(now_time() + ' [WARNING]  未知错误，目标可能拒绝访问', style='bold red')
         
